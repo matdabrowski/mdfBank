3b5f4b0fc8719eb6de8c0773c10a1fae171a5e4e
Mateusz Dąbrowski <mateusz.dabrowski7@gmail.com>
2017-10-22 01:21:56 +0200

    added Turbine in AdminConsole and fixed services configuration for console

--- adminConsole/src/main/java/mdf/bank/adminconsole/AdminConsoleApplication.java
+++ adminConsole/src/main/java/mdf/bank/adminconsole/AdminConsoleApplication.java
@@ -3,9 +3,11 @@ package mdf.bank.adminconsole;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableTurbine
 public class AdminConsoleApplication {
 
 	public static void main(String[] args) {
